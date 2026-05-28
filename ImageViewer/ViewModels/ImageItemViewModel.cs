7fcd6049504f50fca2217ad4b94387e99aa8f494
Astroth <denys.goral@gmail.com>
2026-05-28 10:19:40 +0300

    feat: added new methotd for testing porpouses

--- ImageViewer/ViewModels/ImageItemViewModel.cs
+++ ImageViewer/ViewModels/ImageItemViewModel.cs
@@ -10,4 +10,17 @@ public partial class ImageItemViewModel : ViewModelBase
     [ObservableProperty] private string _resolution;
     [ObservableProperty] private DateTime _dateModified;
     [ObservableProperty] private Bitmap? _imageSource; 
+    
+    // only for tests!!!
+    public ImageItemViewModel(string filePath, string res, DateTime date)
+    {
+        FileName = System.IO.Path.GetFileName(filePath);
+        Resolution = res;
+        DateModified = date;
+
+        if (System.IO.File.Exists(filePath))
+        {
+            ImageSource = new Bitmap(filePath);
+        }
+    }
 }
