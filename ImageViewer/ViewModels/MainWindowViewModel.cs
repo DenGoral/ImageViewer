e8f8ffb4767324d64b5009bb93e26c8e02421f06
Astroth <denys.goral@gmail.com>
2026-05-28 10:27:44 +0300

    feat: added images to Images ObservableCollection

--- ImageViewer/ViewModels/MainWindowViewModel.cs
+++ ImageViewer/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
-using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ImageViewer.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public ObservableCollection<Image> Images { get; } = new();
+    public ObservableCollection<ImageItemViewModel> Images { get; } = new();
+
+    public MainWindowViewModel()
+    {
+        Images.Add(new ImageItemViewModel("/Users/DenysGoral/Programming/ImageViewer/ImageViewer/Assets/Images/1.png", "1920x1080", DateTime.Now));
+        Images.Add(new ImageItemViewModel("/Users/DenysGoral/Programming/ImageViewer/ImageViewer/Assets/Images/2.png", "1920x1080", DateTime.Now));
+        Images.Add(new ImageItemViewModel("/Users/DenysGoral/Programming/ImageViewer/ImageViewer/Assets/Images/3.png", "1920x1080", DateTime.Now));
+        
+    }
 }
